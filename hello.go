5d767639aac8862505889442cf40972285928e1f
Sean Kenny <sean.kenny@hotmail.com>
2018-05-04 13:52:08 +0100

    Add option for random delay

--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +14,17 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	if len(name) == 0 {
 		name = "World"
 	}
+
+	wait := os.Getenv("ADD_JITTER")
+	if len(wait) > 0 {
+		s1 := rand.NewSource(time.Now().UnixNano())
+		rand1 := rand.New(s1)
+
+		r := rand1.Intn(100)
+		r += 100
+		time.Sleep(time.Duration(r) * time.Millisecond)
+	}
+
 	greeting := fmt.Sprintf("Hello %s!", name)
 
 	io.WriteString(w, greeting)
